5c1adf21bbb97c81530a3a07a429b4654c175e67
alydumdolycom <adumitru@dolycom.ro>
2025-12-17 17:24:23 +0200

    update

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -3,12 +3,12 @@ import jwt from "jsonwebtoken";
 export function auth(req, res, next) {
   const token = req.headers.authorization?.split(" ")[1];
 
-  if (!token) return res.status(401).json({ message: "Missing token" });
+  if (!token) return res.status(401).json({ message: "Va rugam sa va autentificati" });
 
   try {
     req.user = jwt.verify(token, process.env.JWT_SECRET);
     next();
   } catch (err) {
-    return res.status(401).json({ message: "Invalid token" });
+    return res.status(401).json({ message: "Token invalid" });
   }
 }
